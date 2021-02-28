ef0ee91a026a18adaa375e23adc743a81e3f8668
ZARIFMAC <uzbzarif@gmail.com>
2021-02-28 20:28:29 +0900

    country flags

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask , render_template
 from jinja2 import Template
+import json
 app = Flask(__name__)
 
-
-
+country = json.loads(open('country.json').read())
+print(country[0]['abbreviation'])
 @app.route('/home')
 @app.route('/')
 def home():
     title = 'Home page'
     content = 'Home page'
     data = {'name':'Zarif','age':33}
+    img_url = 'uz.png'
 
 
-    return render_template('home.html',title=title,content=content,data=data)
+    return render_template('home.html',title=title,content=content,data=data,img_url=img_url)
 
 @app.route('/about')
 def about():
