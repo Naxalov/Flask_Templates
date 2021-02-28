29bde966f02d58038a9cc37e904e46ab5ff85fa2
ZARIFMAC <uzbzarif@gmail.com>
2021-02-28 20:14:07 +0900

    if for

--- app.py
+++ app.py
@@ -2,40 +2,25 @@ from flask import Flask , render_template
 from jinja2 import Template
 app = Flask(__name__)
 
-html_tmp = Template('''
-<html>
-<head>
-    <title>{{title}}</title>
-</head>
-<body>
-    <h1> {{content}}</h1>
 
-    <a href='/home'>home</a>
-    <a href='/about'>about</a>
-    <a href='/product'>product</a>
-    <a href='/contact'>contact</a>
-    <a href='/help'>help</a>
-
-    </body>
-</html>
-''')
 
 @app.route('/home')
 @app.route('/')
 def home():
     title = 'Home page'
     content = 'Home page'
+    data = {'name':'Zarif','age':33}
 
 
-    return render_template('home.html',title='TITLE')
+    return render_template('home.html',title=title,content=content,data=data)
 
 @app.route('/about')
 def about():
     title = 'about page'
     content = 'about page'
 
+    return render_template('home.html',title=title,content=content)
 
-    return html_tmp.render(title=title,content=content)
 
 
 @app.route('/product')
@@ -43,20 +28,21 @@ def product():
     title = 'product page'
     content = 'product page'
 
-    return html_tmp.render(title=title,content=content)
+    return render_template('home.html',title=title,content=content)
+
 
 @app.route('/contact')
 def contact():
     title = 'contact page'
     content = 'contact page'
 
-    return html_tmp.render(title=title,content=content)
+    return render_template('home.html',title=title,content=content)
 
 @app.route('/help')
 def help():
     title = 'help page'
     content = 'help page'
 
-    return html_tmp.render(title=title,content=content)
+    return render_template('home.html',title=title,content=content)
 
 app.run(debug=True)
