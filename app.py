3d2a85edf607f565e8eb23f173069788fd2e16b8
ZARIFMAC <uzbzarif@gmail.com>
2021-02-28 19:29:31 +0900

    Pages

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask 
 app = Flask(__name__)
+html_home = '<h1>Home page</h1>'
+
 @app.route('/')
 def home():
-    return 'Home page'
+    return html_home
+
+@app.route('/product')
+def product():
+    return html_home
+
+@app.route('/contact')
+def contact():
+    return html_home
+
+@app.route('/about')
+def about():
+    return html_home
+
+@app.route('/help')
+def help():
+    return html_home
 
 app.run(debug=True)
