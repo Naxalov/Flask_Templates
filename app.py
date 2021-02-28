0debdecdc962540413efb67c3b7e9f4ede94482b
ZARIFMAC <uzbzarif@gmail.com>
2021-02-28 19:59:26 +0900

    templates

--- app.py
+++ app.py
@@ -1,70 +1,62 @@
-from flask import Flask 
+from flask import Flask , render_template
 from jinja2 import Template
 app = Flask(__name__)
 
+html_tmp = Template('''
+<html>
+<head>
+    <title>{{title}}</title>
+</head>
+<body>
+    <h1> {{content}}</h1>
 
+    <a href='/home'>home</a>
+    <a href='/about'>about</a>
+    <a href='/product'>product</a>
+    <a href='/contact'>contact</a>
+    <a href='/help'>help</a>
 
+    </body>
+</html>
+''')
+
+@app.route('/home')
 @app.route('/')
 def home():
     title = 'Home page'
     content = 'Home page'
 
-    link = '''
-    <a href='/about'>about</a>
-    <a href='/product'>product</a>
-    <a href='/contact'>contact</a>
-    <a href='/help'>help</a>
-    '''
-    html_tmp = f'''
-    <html>
-    <head>
-        <title>{title}</title>
-    </head>
-    <body>
-       <h1> {content}</h1>
-        {link}
-    </body>
-    </html>
-    '''
 
-    return html_tmp
+    return render_template('home.html',title='TITLE')
 
 @app.route('/about')
 def about():
     title = 'about page'
     content = 'about page'
 
-    link = '''
-    <a href='/about'>about</a>
-    <a href='/product'>product</a>
-    <a href='/contact'>contact</a>
-    <a href='/help'>help</a>
-    '''
-    html_tmp = f'''
-    <html>
-    <head>
-        <title>{title}</title>
-    </head>
-    <body>
-       <h1> {content}</h1>
-        {link}
-    </body>
-    </html>
-    '''
 
-    return html_tmp
+    return html_tmp.render(title=title,content=content)
 
 
 @app.route('/product')
 def product():
-    return html_home
+    title = 'product page'
+    content = 'product page'
+
+    return html_tmp.render(title=title,content=content)
 
 @app.route('/contact')
 def contact():
-    return html_home
+    title = 'contact page'
+    content = 'contact page'
+
+    return html_tmp.render(title=title,content=content)
 
 @app.route('/help')
 def help():
-    return html_home
+    title = 'help page'
+    content = 'help page'
+
+    return html_tmp.render(title=title,content=content)
 
 app.run(debug=True)
